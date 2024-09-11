7dea42abd8c6ce9ed1932d32da9f7bc9609bddb2
Chandra Sekhar <gchandrasekhar72@gmail.com>
2024-09-11 06:51:16 +0530

    feat: implement or modified code

---
+++ colors.ts
@@ -0,0 +1,47 @@
+export const COLORS = [
+    {
+        name: "Azure Radiance", color: "#0474fc", colors: [
+            { name: "100", code: "#d6f1ff" },
+            { name: "200", code: "#b6e8ff" },
+            { name: "300", code: "#84dbff" },
+            { name: "400", code: "#21a6ff" },
+            { name: "500", code: "#0474fc" },
+            { name: "600", code: "#0a58c3" },
+            { name: "700", code: "#0f4d99" },
+        ]
+    },
+    {
+        name: "Commodore", color: "#25476a", colors: [
+            { name: "100", code: "#e7eef7" },
+            { name: "200", code: "#cadbed" },
+            { name: "300", code: "#9abedf" },
+            { name: "400", code: "#306499" },
+            { name: "500", code: "#28517c" },
+            { name: "600", code: "#25476a" },
+            { name: "700", code: "#233c57" },
+        ]
+    },
+    {
+        name: "Sea Green", color: "#15803d", colors: [
+            { name: "100", code: "#dcfce8" },
+            { name: "200", code: "#bbf7d1" },
+            { name: "300", code: "#86efad" },
+            { name: "400", code: "#22c55f" },
+            { name: "500", code: "#15803d" },
+            { name: "600", code: "#166534" },
+            { name: "700", code: "#14532c" },
+        ]
+    },
+    {
+        name: "Joyful Orange", color: "#fb923c", colors: [
+            { name: "100", code: "#ffe8d5" },
+            { name: "200", code: "#fed0aa" },
+            { name: "300", code: "#fdb274" },
+            { name: "400", code: "#f78e38" },
+            { name: "500", code: "#fb923c" },
+            { name: "600", code: "#f97c16" },
+            { name: "700", code: "#ea700c" },
+        ]
+    },
+]
+
